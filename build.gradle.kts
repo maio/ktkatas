2931a5cd65601674ca4e7392e36b22aea1064683
Marian Schubert <marian.schubert@gmail.com>
2020-08-12 13:10:28 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "1.4.0-rc"
+}
+
+group = "com.bitsafe"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation("org.assertj:assertj-core:3.11.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+}
+
+tasks {
+    withType<Test> {
+        useJUnitPlatform()
+    }
+}
