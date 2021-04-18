2546a1e5dd824723f2a0e6dc690420bfbdf5b01c
Marian Schubert <marian.schubert@gmail.com>
2021-04-18 10:57:09 +0200

    Update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,20 @@
 plugins {
-    kotlin("jvm") version "1.4.0-rc"
+    kotlin("jvm") version "1.4.32"
 }
 
 group = "com.bitsafe"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    maven("https://dl.bintray.com/kotlin/kotlin-eap")
     mavenCentral()
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
-    testImplementation("org.assertj:assertj-core:3.11.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
-    testImplementation("com.approvaltests:approvaltests:9.3.0")
+    testImplementation("org.assertj:assertj-core:3.19.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+    testImplementation("com.approvaltests:approvaltests:10.4.0")
 
 }
 
